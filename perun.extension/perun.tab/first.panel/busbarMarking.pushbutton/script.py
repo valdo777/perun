e2598a42a1ea35b007284542d5df7db3c11c2999
Vlad Sozutov <vladislav.sozutov@gmail.com>
2021-10-21 22:30:44 +0300

    create def find_next

--- perun.extension/perun.tab/first.panel/busbarMarking.pushbutton/script.py
+++ perun.extension/perun.tab/first.panel/busbarMarking.pushbutton/script.py
@@ -1,13 +1,6 @@
 # coding: utf8
-from math import pi, acos
-
-from Autodesk.Revit.DB import Line, ViewSection, XYZ, FilteredElementCollector, Grid, ReferencePlane, FamilyInstance, \
-    BuiltInParameter, ElevationMarker, ViewType, BuiltInCategory, Transaction
-from Autodesk.Revit.UI.Selection import ObjectType
-from Autodesk.Revit import Exceptions
-
+from Autodesk.Revit.DB import FilteredElementCollector, BuiltInParameter, BuiltInCategory
 import rpw
-from pyrevit import forms, script
 
 __doc__ = """Маркирует элементы шинопровода по порядку"""
 __title__ = "Маркировка\nшинопровода"
@@ -16,52 +9,74 @@ __highlight__ = 'new'
 
 uidoc = rpw.revit.uidoc
 doc = rpw.revit.doc
-logger = script.get_logger()
+
+fittings = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_DuctFitting).WhereElementIsNotElementType().ToElements()
+
+count = 0
+def find_next(element):
+    global count
+    count += 1
+    print(count)
+    try:
+        print("Instance")
+        #Instance
+        connectors = element.MEPModel.ConnectorManager.Connectors
+        if element.Name == "start":
+            #print("START")
+            for connector in connectors:    
+                for ref in connector.AllRefs:
+                    if str(type(ref.Owner)) == "<type 'Duct'>":
+                        print(ref.Owner)
+                        return find_next(ref.Owner)
+            #ref = connectors[0].AllRefs[0]
+            # if str(type(ref.Owner)) == "<type 'Duct'>":
+            #     print(ref.Owner)
+            #     return find_next(ref.Owner)
+        elif element.Name == "finish":
+            # for connector in connectors:    
+            #     for ref in connector.AllRefs:
+            #         if str(type(ref.Owner)) == "<type 'Duct'>":
+            print("finish")
+            return 0
+        else:
+            for connector in connectors:    
+                for ref in connector.AllRefs:
+                    if str(type(ref.Owner)) == "<type 'Duct'>" and str(ref.Direction) == "Out":
+                        return find_next(ref.Owner)
+    except:
+        pass
+
+    try:
+        print("Duct")
+        #Duct
+        connectors = element.ConnectorManager.Connectors
+        for connector in connectors:    
+            for ref in connector.AllRefs:
+                if str(type(ref.Owner)) == "<type 'FamilyInstance'>" and str(ref.Direction) == "Out":
+                    return find_next(ref.Owner)
+    except:
+        pass
 
 
 fittings = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_DuctFitting).WhereElementIsNotElementType().ToElements()
-elementsPast = []
-secondElements = []
-
 for fitting in fittings:
     if fitting.Name == "start":
-        elementsPast.append(fitting.Name)
-        SystemName = fitting.get_Parameter(BuiltInParameter.RBS_SYSTEM_NAME_PARAM).AsString()
-        #print(SystemName)
-        connectors = fitting.MEPModel.ConnectorManager.Connectors
-        for connector in connectors:
-            for ref in connector.AllRefs:
-                if str(type(ref.Owner)) == "<type 'Duct'>":
-                    secondElements.append(ref.Owner)
-                    #print(ref.Owner)
-                    #for i in el.MEPSystem.DuctNetwork:
-                        #print(i)
-                        #logger.debug("Ошибка! здесь")
-
-
-for i in secondElements:
-    for k in i.MEPSystem.DuctNetwork:
-        print(k)
-        if k.Name not in elementsPast:
-            print(str(k.Id) + " - not Past")
-        else:
-            elementsPast.append(k)
-
-    # for j in range(7):
-    #     try:
-    #         connectors = i.ConnectorManager.Connectors
-    #         for connector in connectors:
-    #             for ref in connector.AllRefs:
-    #                 if str(type(ref.Owner)) == "<type 'Duct'>":
-    #                     element = ref.Owner
-    #     except:
-    #         print("notDuct")
-
-    #     try:
-    #         connectors = i.MEPModel.ConnectorManager.Connectors
-    #         for connector in connectors:
-    #             for ref in connector.AllRefs:
-    #                 if str(type(ref.Owner)) == "<type 'FamilyInstance'>":
-    #                     element = ref.Owner
-    #     except:
-    #         print("notFamilyInstance")
+        find_next(fitting)
+    
+    # if fitting.Name == "start":
+    #     connectors = fitting.MEPModel.ConnectorManager.Connectors
+    #     for connector in connectors:
+    #         for ref in connector.AllRefs:
+    #         	print(ref.Owner)
+    #             if str(type(ref.Owner)) == "<type 'Duct'>":
+    #                 secondElements.append(ref.Owner)
+    #                 #print(ref.Owner)
+    #                 if ref.Owner == 1:
+    #                 	print("YES " + str(ref.Owner))
+    #                 for contr in ref.Owner.ConnectorManager.Connectors:
+    #                 	#print(contr.AllRefs)
+    #                 	for i in contr.AllRefs:
+    #                 		#print(i.Owner)
+    #                 		if str(type(i.Owner)) == "<type 'FamilyInstance'>":
+    #                 			if str(i.Direction) == "Out":
+    #                 				pass
